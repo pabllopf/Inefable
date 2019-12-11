46efaf12f4263a9ce15445971fbcab705543d9f5
Pablo Perdomo Falcon <pabloperdomofalcon@gmail.com>
2019-12-11 12:00:47 +0000

    Add Pause Menu script.

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,10 @@
+﻿//------------------------------------------------------------------------------------------
+// <author>Pablo Perdomo Falcón</author>
+// <copyright file="PauseMenu.cs" company="Pabllopf">GNU General Public License v3.0</copyright>
+//------------------------------------------------------------------------------------------
+using UnityEngine;
+
+/// <summary>Manage the pause menu of the game.</summary>
+public class PauseMenu : MonoBehaviour
+{
+}
