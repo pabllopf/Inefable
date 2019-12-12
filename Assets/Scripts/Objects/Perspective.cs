eb8a51e666fde21c404570bc644b6e4185c04823
Pablo Perdomo Falcon <pabloperdomofalcon@gmail.com>
2019-12-12 13:38:03 +0000

    Upgrade town & prefabs.

---
+++ Assets/Scripts/Objects/Perspective.cs
@@ -0,0 +1,49 @@
+﻿//-----------------------------------------------------------------------
+// <author>Pablo Perdomo Falcón</author>
+// <copyright file="Perspective.cs" company="UnMedioStudio">Open Source</copyright>
+//-----------------------------------------------------------------------
+using UnityEngine;
+
+/// <summary>Class to control a column.</summary>
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Occlusion))]
+public class Perspective : MonoBehaviour
+{
+    /// <summary>The order layer front</summary>
+    [SerializeField]
+    private int orderLayerFront = 2;
+
+    /// <summary>The order layer back</summary>
+    [SerializeField]
+    private int orderLayerBack = 5;
+
+    /// <summary>The sprite renderer</summary>
+    private SpriteRenderer spriteRenderer;
+
+    /// <summary>Starts this instance.</summary>
+    private void Start()
+    {
+        this.spriteRenderer = this.GetComponent<SpriteRenderer>();
+    }
+
+    /// <summary>Called when [trigger stay2 d].</summary>
+    /// <param name="obj">The object.</param>
+    private void OnTriggerStay2D(Collider2D obj)
+    {
+        if (obj.CompareTag("Player"))
+        {
+            this.spriteRenderer.sortingOrder = this.orderLayerBack;
+        }
+    }
+
+    /// <summary>Called when [trigger exit2 d].</summary>
+    /// <param name="obj">The object.</param>
+    private void OnTriggerExit2D(Collider2D obj)
+    {
+        if (obj.CompareTag("Player"))
+        {
+            this.spriteRenderer.sortingOrder = this.orderLayerFront;
+        }
+    }
+}
