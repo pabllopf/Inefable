4ff8cdce32bc87b3d68ef94384c8220388a0d3e2
Pablo Perdomo Falcon <pabloperdomofalcon@gmail.com>
2019-12-21 17:50:52 +0000

    First test of the project.

--- Assets/Scripts/UI/Icon.cs
+++ Assets/Scripts/UI/Icon.cs
@@ -2,19 +2,56 @@
 // <author>Pablo Perdomo Falcón</author>
 // <copyright file="Icon.cs" company="Pabllopf">GNU General Public License v3.0</copyright>
 //------------------------------------------------------------------------------------------
+using NUnit.Framework;
+using System;
+using System.Collections;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
 
 /// <summary>Default icon of a object.</summary>
 public class Icon : MonoBehaviour
 {
+    /// <summary>The icon name</summary>
+    [SerializeField] 
+    private string iconName;
+
     /// <summary>The icon</summary>
     [SerializeField]
-    private Sprite icon = null;
+    private Sprite iconSprite = null;
+
+    /// <summary>Awakes this instance.</summary>
+    public void Awake()
+    {
+        this.iconSprite = Resources.Load<Sprite>("Icons/" + this.iconName);
+    }
 
     /// <summary>Gets the icon.</summary>
     /// <returns>Return the icon.</returns>
     public Sprite GetIcon() 
     {
-        return this.icon;
+        return this.iconSprite;
+    }
+
+    public String GetIconName()
+    {
+        return this.iconName;
+    }
+}
+
+public class IconTests
+{
+    [UnityTest]
+    public IEnumerator Test_Load_Icon()
+    {
+        SceneManager.LoadScene("House");
+        yield return null;
+        Icon[] icons = GameObject.FindObjectsOfType<Icon>();
+        foreach (Icon icon in icons) 
+        {
+            Assert.IsNotNull(icon.GetIcon(), "Icon of "+ icon.gameObject.name + " is null.");
+            Assert.AreEqual(icon.GetIcon().name, icon.GetIconName());
+        }
     }
 }
