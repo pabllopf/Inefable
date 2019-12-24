af154ade85e9fd4ccaec1b194271f1b82002165f
Pablo Perdomo Falcon <pabloperdomofalcon@gmail.com>
2019-12-24 17:13:08 +0000

    Refactoring sprites of town.

---
+++ Assets/Tests/Occlusion_.cs
@@ -0,0 +1,18 @@
+﻿//------------------------------------------------------------------------------------------
+// <author>Pablo Perdomo Falcón</author>
+// <copyright file="Occlusion_.cs" company="Pabllopf">GNU General Public License v3.0</copyright>
+//------------------------------------------------------------------------------------------
+using System.Collections;
+using UnityEngine.TestTools;
+
+/// <summary>Test the occlusion class</summary>
+public class Occlusion_
+{
+    /// <summary>Tests 1</summary>
+    /// <returns>Return none</returns>
+    [UnityTest]
+    public IEnumerator Test_1()
+    {
+        yield return null;
+    }
+}
