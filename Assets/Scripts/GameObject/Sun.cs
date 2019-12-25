9268941ecaada0f253041676cb18b87d8e0ea7b9
Pablo Perdomo Falcon <pabloperdomofalcon@gmail.com>
2019-12-25 13:16:11 +0000

    Fixed some prefabs.

---
+++ Assets/Scripts/GameObject/Sun.cs
@@ -0,0 +1,10 @@
+﻿//------------------------------------------------------------------------------------------
+// <author>Pablo Perdomo Falcón</author>
+// <copyright file="Sun.cs" company="Pabllopf">GNU General Public License v3.0</copyright>
+//------------------------------------------------------------------------------------------
+using UnityEngine;
+
+/// <summary>Manage the sun of the game.</summary>
+public class Sun : MonoBehaviour
+{
+}
