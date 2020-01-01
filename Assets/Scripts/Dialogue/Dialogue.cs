bc342f3937b8e119787a4fe4942e9d462538d7f4
Pablo Perdomo Falcon <pabloperdomofalcon@gmail.com>
2020-01-01 13:48:05 +0000

    Refactor Dialogue script.

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -21,35 +21,20 @@ public class Dialogue
 
     /// <summary>Get the name.</summary>
     /// <returns>The name.</returns>
-    public string GetName() 
-    {
-        return this.name;
-    }
+    public string GetName() => this.name;
 
     /// <summary>Gets the sentence.</summary>
     /// <returns>the sentence</returns>
-    public string GetSentence()
-    {
-        return Language.GetSentence(this.sentences[this.index]);
-    }
+    public string GetSentence() => Language.GetSentence(this.sentences[this.index]);
 
     /// <summary>Determines whether this instance has next.</summary>
     /// <returns>
     /// <c>true</c> if this instance has next; otherwise, <c>false</c>.</returns>
-    public bool HasNext()
-    {
-        return (this.index < this.sentences.Length) ? true : false;
-    }
+    public bool HasNext() => (this.index < this.sentences.Length) ? true : false;
 
     /// <summary>Next this instance.</summary>
-    public void Next() 
-    {
-        this.index++;
-    }
+    public void Next() => this.index++;
 
     /// <summary>Resets this instance.</summary>
-    public void Reset()
-    {
-        this.index = 0;
-    }
+    public void Reset() => this.index = 0;
 }
