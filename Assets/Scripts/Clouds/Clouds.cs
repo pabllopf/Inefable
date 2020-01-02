0e0d74070718f36c623075575b726709c2d4d6fe
Pablo Perdomo Falcon <pabloperdomofalcon@gmail.com>
2020-01-02 20:07:17 +0000

    Refactor Clouds script.

--- Assets/Scripts/Clouds/Clouds.cs
+++ Assets/Scripts/Clouds/Clouds.cs
@@ -2,6 +2,7 @@
 // <author>Pablo Perdomo Falcón</author>
 // <copyright file="Clouds.cs" company="Pabllopf">GNU General Public License v3.0</copyright>
 //------------------------------------------------------------------------------------------
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,10 +15,7 @@ public class Clouds : MonoBehaviour
 
     /// <summary>The time to change direction</summary>
     [SerializeField]
-    private float timeToChangeDir = 0;
-
-    /// <summary>The time reset</summary>
-    private float timeReset = 0;
+    private float timeToChange = 0;
 
     /// <summary>The current clouds</summary>
     private int currentClouds = 0;
@@ -47,7 +45,6 @@ public class Clouds : MonoBehaviour
     /// <summary>Starts this instance.</summary>
     private void Start()
     {
-        this.timeReset = this.timeToChangeDir;
         this.CreateClouds();
         this.Move();
     }
@@ -55,24 +52,17 @@ public class Clouds : MonoBehaviour
     /// <summary>Creates the clouds.</summary>
     private void CreateClouds() 
     {
-        if (this.clouds.Count > 0)
+        this.CreateGroups();
+        while (this.currentClouds <= this.maxClouds)
         {
-            this.CreateGroups();
-            while (this.currentClouds <= this.maxClouds) 
-            {
-                int cloudNum = Random.Range(0, this.clouds.Count);
-                GameObject cloudSpawned = MonoBehaviour.Instantiate(this.clouds[cloudNum], new Vector3(Random.Range(this.xRange.x, this.xRange.y), Random.Range(this.yRange.x, this.yRange.y), 0), Quaternion.identity);
-                cloudSpawned.transform.parent = this.groups[Random.Range(0, this.groups.Count)].transform;
-                int size = Random.Range(4, 11);
-                cloudSpawned.transform.localScale = new Vector3(size, size, 0);
-                cloudSpawned.GetComponent<SpriteRenderer>().sortingOrder = Random.Range(-3, -1);
-                
-                this.currentClouds++;
-            }
-        }
-        else 
-        {
-            Debug.Log("Please insert clouds prefabs to spawn.");
+            int cloudNum = Random.Range(0, this.clouds.Count);
+            GameObject cloudSpawned = MonoBehaviour.Instantiate(this.clouds[cloudNum], new Vector3(Random.Range(this.xRange.x, this.xRange.y), Random.Range(this.yRange.x, this.yRange.y), 0), Quaternion.identity);
+            cloudSpawned.transform.parent = this.groups[Random.Range(0, this.groups.Count)].transform;
+            int size = Random.Range(4, 11);
+            cloudSpawned.transform.localScale = new Vector3(size, size, 0);
+            cloudSpawned.GetComponent<SpriteRenderer>().sortingOrder = Random.Range(-3, -1);
+
+            this.currentClouds++;
         }
     }
 
@@ -92,23 +82,20 @@ public class Clouds : MonoBehaviour
     /// <summary>Updates this instance.</summary>
     private void Update()
     {
-        if (this.timeToChangeDir <= 0)
-        {
-            this.timeToChangeDir = this.timeReset;
-            this.Move();
-        }
-        else 
-        {
-            this.timeToChangeDir -= Time.deltaTime;
-        }
-
         this.currentGroup.transform.position = Vector3.LerpUnclamped(this.currentGroup.transform.position, this.currentGroupPos, 0.05f * Time.deltaTime);
     }
 
     /// <summary>Moves this instance.</summary>
     private void Move()
     {
+        this.StartCoroutine(this.MoveControl(this.timeToChange));
+    }
+
+    private IEnumerator MoveControl(float time) 
+    {
         this.currentGroup = this.groups[Random.Range(0, this.groups.Count)];
         this.currentGroupPos = this.currentGroup.transform.position + new Vector3(Random.Range(-10, 10), 0, 0);
+        yield return new WaitForSeconds(time);
+        this.Move();
     }
 }
