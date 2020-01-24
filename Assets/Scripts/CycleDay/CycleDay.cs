1c775ed6b2fe356262b27cd8f9a6288f3cbd1be3
Pablo <pabloperdomofalcon@gmail.com>
2020-01-24 10:38:39 +0000

    Add the new Bridges

---
+++ Assets/Scripts/CycleDay/CycleDay.cs
@@ -0,0 +1,12 @@
+﻿//------------------------------------------------------------------------------------------
+// <author>Pablo Perdomo Falcón</author>
+// <copyright file="CycleDay.cs" company="Pabllopf">GNU General Public License v3.0</copyright>
+//------------------------------------------------------------------------------------------
+using UnityEngine;
+
+/// <summary>Manage the cycle of day and night</summary>
+public class CycleDay : MonoBehaviour
+{
+    /// <summary>Starts this instance.</summary>
+    private void Start() => GameObject.Find("Camera").GetComponent<Camera>().backgroundColor = new Color(192, 192, 192, 0);
+}
