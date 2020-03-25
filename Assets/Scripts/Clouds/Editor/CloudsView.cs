cfdd955dd381243acfd8ca88cd5886fbc3c65371
Pablo <pabloperdomofalcon@gmail.com>
2020-03-25 11:22:55 +0000

    Add fps counter and clouds generator.

---
+++ Assets/Scripts/Clouds/Editor/CloudsView.cs
@@ -0,0 +1,96 @@
+﻿//------------------------------------------------------------------------------------------
+// <author>Pablo Perdomo Falcón</author>
+// <copyright file="CloudsView.cs" company="Pabllopf">GNU General Public License v3.0</copyright>
+//------------------------------------------------------------------------------------------
+namespace CloudsGenerator.Editor
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using UnityEditor;
+    using UnityEngine;
+
+    /// <summary>Clouds generator editor view</summary>
+    [CustomEditor(typeof(Clouds))]
+    public class CloudsView : Editor
+    {
+        /// <summary>Implement this function to make a custom inspector.</summary>
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            if (GUILayout.Button("Update"))
+            {
+                Clouds clouds = target as Clouds;
+
+                LoadClouds(ref clouds);
+
+                UnityEditor.EditorApplication.delayCall += () =>
+                {
+                    AssetDatabase.Refresh();
+                };
+            }
+
+            base.OnInspectorGUI();
+        }
+
+        /// <summary>Loads the clouds.</summary>
+        /// <param name="clouds">The clouds.</param>
+        private void LoadClouds(ref Clouds clouds) 
+        {
+            clouds.CloudsPrefabs = UpdateField("/Clouds");
+        }
+
+        /// <summary>Updates the field.</summary>
+        /// <param name="path">The path.</param>
+        /// <returns>List of objects</returns>
+        private List<GameObject> UpdateField(string path)
+        {
+            string prefabDir = Application.dataPath + "/Prefabs";
+            if (!Directory.Exists(prefabDir))
+            {
+                Directory.CreateDirectory(prefabDir);
+                Debug.Log("Directory created at: " + prefabDir);
+            }
+
+            string pathFiles = Application.dataPath + "/Prefabs/" + path;
+            if (!Directory.Exists(pathFiles))
+            {
+                Directory.CreateDirectory(pathFiles);
+                Debug.Log("Directory created at: " + pathFiles);
+            }
+
+            List<GameObject> result = new List<GameObject>();
+            if (Directory.Exists(pathFiles))
+            {
+                Directory.GetFiles(pathFiles)
+                    .ToList()
+                    .FindAll(file => Path.GetExtension(file) == ".prefab")
+                    .ForEach(filePath =>
+                    {
+                        string dirOfTiles = "Assets/Prefabs/" + path;
+                        if (!Directory.Exists(dirOfTiles))
+                        {
+                            Directory.CreateDirectory(dirOfTiles);
+                            Debug.Log("Directory created at: " + dirOfTiles);
+                        }
+
+                        string objPath = "Assets/Prefabs/" + path + "/" + Path.GetFileName(filePath);
+                        GameObject obj = (GameObject)AssetDatabase.LoadAssetAtPath(objPath, typeof(GameObject));
+
+                        if (!result.Contains(obj))
+                        {
+                            result.Add(obj);
+                        }
+                    });
+            }
+
+            if (result.Count <= 0)
+            {
+                Debug.LogError("Directory of clouds is empty: " + "Assets/Prefabs/" + path);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Assets/Scripts/Clouds/Runtime/Clouds.cs
@@ -0,0 +1,73 @@
+﻿//------------------------------------------------------------------------------------------
+// <author>Pablo Perdomo Falcón</author>
+// <copyright file="Clouds.cs" company="Pabllopf">GNU General Public License v3.0</copyright>
+//------------------------------------------------------------------------------------------
+namespace CloudsGenerator
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    /// <summary>Generator of clouds.</summary>
+    public class Clouds : MonoBehaviour
+    {
+        [Header("Configuration:")]
+
+        /// <summary>The maximum number of clouds</summary>
+        [SerializeField]
+        [Range(1, 250)]
+        private int maxNumOfClouds = 100;
+
+        /// <summary>The x position</summary>
+        [SerializeField]
+        private Vector2 xRange = Vector2.zero;
+
+        /// <summary>The y range</summary>
+        [SerializeField]
+        private Vector2 yRange = Vector2.zero;
+
+        /// <summary>The clouds prefabs</summary>
+        [SerializeField]
+        private List<GameObject> cloudsPrefabs = null;
+
+        #region Encapsulate Fields
+
+        /// <summary>Gets or sets the maximum number of clouds.</summary>
+        /// <value>The maximum number of clouds.</value>
+        public int MaxNumOfClouds { get => maxNumOfClouds; set => maxNumOfClouds = value; }
+
+        /// <summary>Gets or sets the x range.</summary>
+        /// <value>The x range.</value>
+        public Vector2 XRange { get => xRange; set => xRange = value; }
+        
+        /// <summary>Gets or sets the y range.</summary>
+        /// <value>The y range.</value>
+        public Vector2 YRange { get => yRange; set => yRange = value; }
+
+        /// <summary>Gets or sets the clouds prefabs.</summary>
+        /// <value>The clouds prefabs.</value>
+        public List<GameObject> CloudsPrefabs { get => cloudsPrefabs; set => cloudsPrefabs = value; }
+       
+        #endregion
+
+        /// <summary>Starts this instance.</summary>
+        private void Start()
+        {
+            if (cloudsPrefabs == null) 
+            {
+                Debug.LogError("Empty clouds list.");
+            }
+
+            while (maxNumOfClouds > 0)
+            {
+                GameObject cloud = cloudsPrefabs[Random.Range(0, cloudsPrefabs.Count - 1)];
+                GameObject cloudSpawned = Instantiate(cloud, new Vector2(Random.Range(xRange.x, xRange.y), Random.Range(yRange.x, yRange.y)), Quaternion.identity, this.transform);
+
+                int size = Random.Range(4, 11);
+                cloudSpawned.transform.localScale = new Vector3(size, size, 0);
+                cloudSpawned.GetComponent<SpriteRenderer>().sortingOrder = Random.Range(-3, -1);
+
+                maxNumOfClouds--;
+            }
+        }
+    }
+}
